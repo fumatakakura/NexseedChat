cb387121676e5f3ad6b9f85b231f427a402323be
高倉楓麻 <ai24cuz@gmail.com>
2019-12-09 15:32:07 +0800

    first commit

---
+++ NexseedChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NexseedChat
+//
+//  Created by 高倉楓麻 on 2019/12/09.
+//  Copyright © 2019 高倉楓麻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
