ca74b7b7fe5e2189b40b54b92f8fe3009a53c34a
高倉楓麻 <ai24cuz@gmail.com>
2019-12-11 13:41:59 +0800

    ログイン機能 チャット部屋追加

---
+++ NexseedChat/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  NexseedChat
+//
+//  Created by 高倉楓麻 on 2019/12/10.
+//  Copyright © 2019 高倉楓麻. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NexseedChat/Objects/Room.swift
@@ -0,0 +1,9 @@
+//
+//  Room.swift
+//  NexseedChat
+//
+//  Created by 高倉楓麻 on 2019/12/10.
+//  Copyright © 2019 高倉楓麻. All rights reserved.
+//
+
+import Foundation
